7f32b11f83e77430cdb138c81900a5727164ba31
heyjordn <proxima.aust@gmail.com>
2023-05-15 13:37:19 -0500

    feat, include name title

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,8 +1,11 @@
 <template>
   <div class="flex w-full pt-2 items-center max-w-3xl mx-auto">
     <div class="w-1/5 md:w-1/2 align-left">
-      <nuxt-link to="/">
-        <logo />
+      <nuxt-link to="/" class="flex items-center">
+        <logo class="pr-5 border-r-2" />
+        <p class="ml-5 text-2xl font-display font-bold text-white">
+          Jordan Jones
+        </p>
       </nuxt-link>
     </div>
     <div class="w-4/5 md:w-1/2 text-right">
