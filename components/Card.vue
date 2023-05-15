6ade083f8598080dbd8b7f7c9c0fe016b0ccb345
heyjordn <proxima.aust@gmail.com>
2023-05-15 13:39:06 -0500

    feat, added project pages

---
+++ components/Card.vue
@@ -0,0 +1,56 @@
+<template>
+  <a
+    :href="link"
+    class="no-underline"
+    target="_blank"
+    rel="noopener noreferrer"
+  >
+    <div class="max-w-xs rounded overflow-hidden shadow-lg bg-white my-2">
+      <img class="w-full" :src="image" :alt="title" />
+      <div class="px-6 py-4">
+        <div class="font-bold text-xl mb-2 text-indigo-700">{{ title }}</div>
+        <p class="text-gray-700 text-base">
+          {{ body }}
+        </p>
+      </div>
+      <div class="px-6 pt-4 pb-2">
+        <span
+          v-for="(tag, index) in tags"
+          :key="index"
+          class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
+        >
+          {{ tag }}
+        </span>
+      </div>
+    </div>
+  </a>
+</template>
+<script>
+export default {
+  props: {
+    link: {
+      type: String,
+      default: 'https://heyjordn.com',
+    },
+    image: {
+      type: String,
+      default: 'https://v1.tailwindcss.com/img/card-top.jpg',
+    },
+    title: {
+      type: String,
+      default: 'Sample Title',
+    },
+    body: {
+      type: String,
+      default:
+        'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.',
+    },
+    tags: {
+      type: Array,
+      default() {
+        return ['vue', 'laravel', 'nuxt']
+      },
+    },
+  },
+}
+</script>
--- pages/about.vue
+++ pages/about.vue
@@ -1,112 +1,108 @@
 <template>
   <div>
-  <div class="flex flex-col sm:flex-row my-6 md:my-4">
-    <div class="text-left">
-      <h1 class="font-bold mb-3">Summary 👋</h1>
-      <p class="mb-3">Jordan Jones is a A Certified DevOps Engineer and a Kubernetes and Cloud Native Associate with extensive Software Engineering experience based in Kingston, Jamaica. Currently working at Orba on the Metro platform. He has over 9 years of professionally developing and deploying platforms for companies in North America and the wider Caribbean.</p>
-      <h2 class="font-bold mb-3">Projects 💻</h2>
-      <ul>
-        <li>
-          <a
-            href="https://voltauto.io/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            voltauto.io
-          </a>
-          <span>
-                - A peer to peer Motor Vehicle marketplace faciliting advertising,buying, selling and trading. Currently still being  developed.
-              </span>
-        </li>
-        <li>
-        <li>
-          <a
-            href="https://metroipo.com/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            metroipo.com
-          </a>
-          <span>
-                - An Initial Public Offer (IPO) underwriting platform designed to
-                interface with the Jamaican Stock Exchange.
-              </span>
-        </li>
-        <li>
-          <a
-            href="https://orbaone.com/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            orbaone.com
-          </a>
-          <span>
-                - An identity verification platform, using machine learning for
-                facial recognition.
-              </span>
-        </li>
-        <li>
-          <a
-            href="https://zafe.app/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            zafe.app
-          </a>
-          <span>
-                - A salary advance platform that reduces friction for employee
-                loans and administrative overhead.
-              </span>
-        </li>
-        <li>
-          <a
-            href="https://staffgenius.io/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            staffgenius.io
-          </a>
-          <span>
-                - A remote workforce management suite of tools.
-              </span>
-        </li>
-      </ul>
+    <div class="flex flex-col sm:flex-row my-6 md:my-4">
+      <div class="text-left">
+        <h1 class="font-bold text-xl mb-3 font-display">Summary 👋</h1>
+        <p class="mb-3">
+          I'm Jordan, A Certified DevOps Engineer and a Kubernetes and Cloud
+          Native Associate with extensive Software Engineering experience based
+          in Kingston, Jamaica. Currently working at Orba on the Metro platform.
+          He has over 9 years of professionally developing and deploying
+          platforms for companies in North America and the wider Caribbean.
+        </p>
+        <h2 class="font-bold mb-3 text-xl font-display">Past Experience 💻</h2>
+        <div class="grid grid-cols-1 sm:grid-cols-2 w-full">
+          <card
+            v-for="(card, index) in cards"
+            :key="index"
+            :title="card.title"
+            :body="card.body"
+            :image="card.image"
+            :link="card.link"
+          />
+        </div>
+      </div>
     </div>
-  </div>
-  <div class="flex flex-col sm:flex-row my-6 md:my-4 items-center">
-    <div class="text-left">
-      <h6 class="font-bold mb-3">Open Source 🌐</h6>
-      <p>
-        I experiment and publish tools and example repos for whatever I'm
-        working on over at my
-        <a href="https://github.com/heyjordn">Github</a>. I find the best
-        way to learn is to reduce complexity and get my hands dirty.
-      </p>
+    <div class="flex flex-col sm:flex-row my-6 md:my-4 items-center">
+      <div class="text-left">
+        <h6 class="font-bold mb-3 text-xl font-display">Open Source 🌐</h6>
+        <p>
+          I experiment and publish tools and example repos for whatever I'm
+          working on over at my
+          <a href="https://github.com/heyjordn">Github</a>. I find the best way
+          to learn is to reduce complexity and get my hands dirty.
+        </p>
+      </div>
     </div>
-  </div>
-  <div class="flex flex-col sm:flex-row my-6 md:my-4 items-center">
-    <div class="text-left">
-      <h6 class="font-bold mb-3">Certs 📜</h6>
-      <p>
-        Love cloud native tech (Kubernetes, Terraform, Ansible, Jenkins,
-        CircleCI) and architecting new solutions and system designs, this
-        lead me to persue a couple of certifications.
-      </p>
-      <ul>
-        <li>
-          <a
-            href="https://www.credly.com/badges/0d965b71-31fd-452d-9544-6b495d16fde0/linked_in?t=rcrcx4"
-          >Jordan Jones - KCNA</a
-          >
-        </li>
-        <li>
-          <a
-            href="https://www.credly.com/badges/8500304b-30fa-43b0-9bc3-8078fbf3e8eb/linked_in?t=rbttzu"
-          >Jordan Jones - LFS250</a
-          >
-        </li>
-      </ul>
+    <div class="flex flex-col sm:flex-row my-6 md:my-4 items-center">
+      <div class="text-left">
+        <h6 class="font-bold mb-3 text-xl font-display">Certs 📜</h6>
+        <p>
+          Love cloud native tech (Kubernetes, Terraform, Ansible, Jenkins,
+          CircleCI) and architecting new solutions and system designs, this lead
+          me to persue a couple of certifications.
+        </p>
+        <ul>
+          <li>
+            <a
+              href="https://www.credly.com/badges/0d965b71-31fd-452d-9544-6b495d16fde0/linked_in?t=rcrcx4"
+              >Jordan Jones - KCNA</a
+            >
+          </li>
+          <li>
+            <a
+              href="https://www.credly.com/badges/8500304b-30fa-43b0-9bc3-8078fbf3e8eb/linked_in?t=rbttzu"
+              >Jordan Jones - LFS250</a
+            >
+          </li>
+        </ul>
+      </div>
     </div>
-  </div>
   </div>
 </template>
+<script>
+export default {
+  data() {
+    return {
+      cards: [
+        {
+          link: 'https://voltauto.io',
+          title: 'Volt Auto',
+          image: 'https://heyjordn.com/volt.png',
+          body: 'A peer to peer Motor Vehicle marketplace faciliting advertising,buying, selling and trading.',
+        },
+        {
+          link: 'https://metroipo.com',
+          title: 'Metro IPO',
+          image: 'https://heyjordn.com/metro.png',
+          body: 'An Initial Public Offer (IPO) underwriting and Private Equity platform designed to interface with the Jamaican Stock Exchange.',
+        },
+        {
+          link: 'https://orba.io/',
+          title: 'Orba',
+          image: 'https://heyjordn.com/orba.png',
+          body: 'A Nuxt 2 static website redesign built for Orba technology holdings',
+        },
+        {
+          link: 'https://orbaone.com/',
+          title: 'Orba One',
+          image: 'https://heyjordn.com/orbaone.png',
+          body: 'An identity verification and KYC platform, using machine learning for facial recognition.',
+        },
+        {
+          link: 'https://zafe.app/',
+          title: 'Zafe',
+          image: 'https://heyjordn.com/zafe.png',
+          body: 'A salary advance platform that reduces friction for employee loans and administrative overhead',
+        },
+        {
+          link: 'https://staffgenius.io/',
+          title: 'StaffGenius',
+          image: 'https://heyjordn.com/staffgenius.png',
+          body: 'A remote workforce management suite of tools.',
+        },
+      ],
+    }
+  },
+}
+</script>
